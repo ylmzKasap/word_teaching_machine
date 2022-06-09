836214be97162d036c17af8c2c67a65c04ec6b7d
Yılmaz Kasap <ylmzkasap@outlook.com>
2022-06-09 14:50:52 +0300

    Configure databases

--- backend/database/development_database.js
+++ backend/database/development_database.js
@@ -1,11 +1,7 @@
-const { Pool } = require("pg");
+const development_db = `postgresql://${process.env.DEVELOPMENT_USER}` 
+  + `:${process.env.DEVELOPMENT_PASSWORD}`
+  + `@${process.env.DEVELOPMENT_HOST}`
+  + `:${process.env.DEVELOPMENT_PORT}`
+  + `/${process.env.DEVELOPMENT_DATABASE}`;
 
-const db = new Pool({
-    user: process.env["DEVELOPMENT_USER"],
-    password: process.env["DEVELOPMENT_PASSWORD"],
-    database: process.env["DEVELOPMENT_DATABASE"],
-    host: process.env["DEVELOPMENT_HOST"],
-    port: process.env["DEVELOPMENT_PORT"]
-});
-
-module.exports = db;
+module.exports = development_db;
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 require('dotenv').config();
-const database = require('./database/development_database');
 const db_tests = require('./database/build_database');
 const startApp = require('./app');
+const development_db = require('./database/development_database');
+const { Pool } = require("pg");
+
+const isProduction = process.env.NODE_ENV === "production";
+const ssl = process.env.NODE_ENV == 'production' ? '?ssl=true' : '';
+
+const database = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL + ssl : development_db
+});
+
 const app = startApp(database);
 
-
 async function main() {
-    await db_tests.teardown(database);
-    await db_tests.setUp(database);
+  await db_tests.teardown(database);
+  await db_tests.setUp(database);
 }
 
 main()
